eb2fbba90d0e7d6ef43e4e428da12e845b914c0e
monegasco <mhoa.2000@gmail.com>
2023-10-19 20:25:23 -0300

    controllers e injecao de dependencia

--- src/DevTrackR.ShippingOrders.API/Program.cs
+++ src/DevTrackR.ShippingOrders.API/Program.cs
@@ -1,7 +1,12 @@
+using DevTrackR.ShippingOrders.Application;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+// Add services to the container.
+builder
+    .Services
+    .AddApplication();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
--- src/DevTrackR.ShippingOrders.Application/InputModels/AddShippingOrderInputModel.cs
+++ src/DevTrackR.ShippingOrders.Application/InputModels/AddShippingOrderInputModel.cs
@@ -1,12 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DevTrackR.ShippingOrders.Core.Entities;
+using DevTrackR.ShippingOrders.Core.ValueObjects;
 
-namespace DevTrackR.ShippingOrders.Application.InputModels
+namespace DevTrackR.ShippingOrders.Application.InputModels;
+
+public class AddShippingOrderInputModel
 {
-    internal class AddShippingOrderInputModel
-    {
-    }
+    public string Description { get; set; }
+    public decimal WeightInKg { get; set; }
+    public DeliveryAddressInputModel DeliveryAddress { get; set; }
+    public List<ShippingServiceInputModel> Services { get; set; }
+
+    public ShippingOrder ToEntity()
+        => new ShippingOrder(
+            Description,
+            WeightInKg,
+            DeliveryAddress.ToValueObject()
+        );
+}
+
+public class DeliveryAddressInputModel
+{
+    public string Street { get; set; }
+    public string Number { get; set; }
+    public string ZipCode { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string Country { get; set; }
+    public string ContactEmail { get; set; }
+    public DeliveryAddress ToValueObject()
+        => new DeliveryAddress(Street, Number, ZipCode, City, State, Country, ContactEmail);
+}
+
+public class ShippingServiceInputModel
+{
+    public string Title { get; set; }
+    public decimal PricePerKg { get; set; }
+    public decimal FixedPrice { get; set; }
+
+    public ShippingService ToEntity()
+        => new ShippingService(Title, PricePerKg, FixedPrice);
 }
