8cd09929bd758c96c7b5eec4987c2df51b60e430
monegasco <mhoa.2000@gmail.com>
2023-10-19 21:33:52 -0300

    persistencia com mongoDb

--- src/DevTrackR.ShippingOrders.API/Program.cs
+++ src/DevTrackR.ShippingOrders.API/Program.cs
@@ -1,4 +1,5 @@
 using DevTrackR.ShippingOrders.Application;
+using DevTrackR.ShippingOrders.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder
     .Services
+    .AddInfrastructure()
     .AddApplication();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
